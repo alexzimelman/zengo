bf6b328387f6fb0e4dd1c4842e3e1304e973c574
Alex <azimelman262@gmail.com>
2021-06-17 21:10:08 +0300

    update

--- src/greeting.js
+++ src/greeting.js
@@ -8,13 +8,14 @@ module.exports =  `
             font-size: 2rem;
             color: #90ff13;
             text-shadow: 1px 1px 15px rgba(158,255,97,0.92), 0px 0px 5px rgba(158,255,255,0.82)">
-                SERVICE IS ALIVE
+                ZENGO API ASSIGNMENT IS ALIVE
             </div>
             <div style="width: 100%; text-align: center; margin-top: 0.8rem; font-size: 1.3rem; color: #efefef">
-                Go do your thing.
+                required params: coins and date
+                exmple query: https://radiant-shelf-78064.herokuapp.com/coins/prices?coins=BTC,ETH,BNB,DOGE&date=01/01/2020
             </div>
             <div style="width: 100%; text-align: center; margin-top: 4rem; font-size: 1rem; color: #989898">
-                OXS
+                Alex Zimelman
             </div>
 </code>
         </body>

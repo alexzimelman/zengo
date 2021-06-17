f8f9ece54147400bb65960eaed716e274b47b7d8
Alex Zimelman <alexzimelman@MacBook-Pro.lan>
2021-06-17 22:30:20 +0300

    update

--- src/routes/CoinsRouter.js
+++ src/routes/CoinsRouter.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const CoinsRouter = express.Router();
 const CoinController = require('../controllers/CoinController')
-const NO_PARAMS_ERR = 'Please choose coins and date. Make sure date is in the right format: MM/DD/YYYY .'
+const NO_PARAMS_ERR = 'Please choose coins and date.'
+const DATE_FORMAT_ERR = 'Date err. Make sure date is in the right format: MM/DD/YYYY .'
 
 async function getCoinsPrices(req, res){
     try{
         let params = req.query
         if(!params || !params.coins || !params.date){
             return res.status(400).json(NO_PARAMS_ERR)
+        }
+        else if(parseInt(params.date.substring(0, 2)) > 12){
+            return res.status(400).json(DATE_FORMAT_ERR)
         }
         let result = await CoinController.getCoinsPrices(params)
         return res.json(result)
